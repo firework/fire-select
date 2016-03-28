54f7065abe84fa43ae6380d77dc9b4b3bb2b6411
MarcosRJJunior <marcosrjjunior@gmail.com>
2016-03-28 11:09:05 -0300

    Adding gh_pages

---
+++ README.md
@@ -0,0 +1,5 @@
+# 🔥_Fireselect_
+
+Vue component that transforms overwhelming `select` boxes into something fancy, simple and user-friendly.
+
+It is similar to Selectize, Chosen, Select2, etc. However it was built using Vue.js only ;)
---
+++ script.js
@@ -0,0 +1,38 @@
+var obj = [
+    {value: 'html', label: 'HyperText Markup Language'},
+    {value: 'css', label: 'Cascading Style Sheets'},
+    {value: 'js', label: 'Javascript'},
+    // {value: 'php', label: 'Hypertext Preprocessor'},
+];
+
+// var languages = ['HyperText Markup Language', 'Cascading Style Sheets', 'Javascript', 'Hypertext Preprocessor'];
+var arr = ['HyperText Markup Language', 'Cascading Style Sheets', 'Javascript'];
+
+var vm = new Vue({
+    el: 'body',
+
+    data: {
+        languagesObj: obj,
+        languages: arr,
+        options: 'object',
+        multiple: false,
+        create: true,
+        animation: true,
+        helperMessage: 'Type anything to search',
+        placeholder: 'Select an item',
+    },
+
+    events: {
+        fsOptionAdded: function(option) {
+            console.log('fire-select option added:', option);
+        },
+
+        fsOptionSelected: function(option) {
+            console.log('fire-select option selected:', option);
+        },
+
+        fsOptionDeselect: function(option) {
+            console.log('fire-select option deselected:', option);
+        },
+    }
+});
